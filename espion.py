9cf09fdb5325ceaa925616cec45545947ec338bc
Amos Wenger <amoswenger@gmail.com>
2022-08-30 17:11:58 +0200

    C'est bien

--- espion.py
+++ espion.py
@@ -1,5 +1,9 @@
 dbt = int(input())
 fin = int(input())
+print(f"L'espion est arrivé entre le {dbt} et le {fin}")
 
-# if (dbt_fin <= dt_entree) and (dbt_fin >= dt_sortie)
-#      print (
+n = int(input())
+print(f"{n} personnes sont rentrées dans la ville en tout")
+
+# for i in range(n):
+#  print(f"Tour de boucle {i}")
