a54772f9f72f338e53171979216273c6ab1b0bd4
Amos Wenger <amoswenger@gmail.com>
2022-08-30 17:17:15 +0200

    Ok mais c'est mieux

--- espion.py
+++ espion.py
@@ -5,5 +5,11 @@ print(f"L'espion est arrivé entre le {dbt} et le {fin}")
 n = int(input())
 print(f"{n} personnes sont rentrées dans la ville en tout")
 
+for i in range(n):
+    date = int(input())
+    if date >= dbt and date <= fin:
+        print(f"La personne qui est rentree le {date} est suspicieuse")
+
+
 # for i in range(n):
 #  print(f"Tour de boucle {i}")
