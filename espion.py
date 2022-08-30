ba048c3e95d797380ccf6e5003a88de2b7345694
Amos Wenger <amoswenger@gmail.com>
2022-08-30 17:24:47 +0200

    La solution

--- espion.py
+++ espion.py
@@ -1,9 +1,9 @@
 dbt = int(input())
 fin = int(input())
-print(f"L'espion est arrivé entre le {dbt} et le {fin}")
+# print(f"L'espion est arrivé entre le {dbt} et le {fin}")
 
 n = int(input())
-print(f"{n} personnes sont rentrées dans la ville en tout")
+# print(f"{n} personnes sont rentrées dans la ville en tout")
 
 nb = 0
 for i in range(n):
